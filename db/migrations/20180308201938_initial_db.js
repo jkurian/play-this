1c22286f48b55a47bed10ce20e70f2a94af831e1
J Rose <josh@upros3.com>
2018-03-09 00:42:37 +0000

    created migration, need to test db with seed

--- db/migrations/20180308201938_initial_db.js
+++ db/migrations/20180308201938_initial_db.js
@@ -2,26 +2,80 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     // create user table
     knex.schema.createTable("users", function(table) {
-      table.increments();
+      table.increments("id");
       table.string("first_name");
       table.string("last_name");
+      table.string("avatar_image");
       table.string("email");
-      table.string("image");
-      // foreign key Request_ID
+      table.string("password");
     }),
-    // requests user table
+
+    // requests table
     knex.schema.createTable("requests", function(table) {
-      table.increments();
+      table.increments("id");
       table.string("name");
       table.string("reason");
-      //foreign key user
+      table.integer("user_admin_id").unsigned();
+      table.foreign("user_admin_id").references("users.id");
+    }),
+
+    // songs table
+    knex.schema.createTable("songs", function(table) {
+      table.increments("id");
+      table.string("artist");
+      table.string("title");
+      table.string("album");
+      table.string("spotifyID");
+      table.integer("request_id").unsigned();
+      table.foreign("request_id").references("requests.id");
+      table.integer("user_id").unsigned();
+      table.foreign("user_id").references("users.id");
+    }),
+
+    // Comments table
+    knex.schema.createTable("comments", function(table) {
+      table.increments("id");
+      table.string("comment");
+      table.integer("user_id").unsigned();
+      table.foreign("user_id").references("users.id");
+      table.integer("song_id").unsigned();
+      table.foreign("song_id").references("songs.id");
+    }),
+
+    // User_song table
+    knex.schema.createTable("user_song", function(table) {
+      table.integer("user_id").unsigned();
+      table.foreign("user_id").references("users.id");
+      table.integer("song_id").unsigned();
+      table.foreign("song_id").references("songs.id");
+    }),
+
+    // User likes table
+    knex.schema.createTable("userLikes", function(table) {
+      table.integer("user_id").unsigned();
+      table.foreign("user_id").references("users.id");
+      table.integer("song_id").unsigned();
+      table.foreign("song_id").references("songs.id");
+    }),
+
+    // User Friends tableName
+    knex.schema.createTable("userFriends", function(table) {
+      table.integer("user_id1").unsigned();
+      table.foreign("user_id1").references("users.id");
+      table.integer("user_id2").unsigned();
+      table.foreign("user_id2").references("users.id");
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable("users"),
-    knex.schema.dropTable("requests")
+    knex.schema.dropTable("userFriends"),
+    knex.schema.dropTable("userLikes"),
+    knex.schema.dropTable("user_song"),
+    knex.schema.dropTable("comments"),
+    knex.schema.dropTable("songs"),
+    knex.schema.dropTable("requests"),
+    knex.schema.dropTable("users")
   ]);
 };
