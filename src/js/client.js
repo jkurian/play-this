dc0b5dfff7d3ff2e00584ed0134f9903791529c2
Jerry Kurian <jkurian770@gmail.com>
2018-03-08 19:30:57 -0500

    NavBar.jsx component complete

--- src/js/client.js
+++ src/js/client.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
 import { Provider } from 'react-redux'
 
@@ -12,7 +14,7 @@ const app = document.getElementById('app')
 
 ReactDOM.render(
 <Provider store={store}>
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <Layout />
     </MuiThemeProvider>
 </Provider>
