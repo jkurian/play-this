07b2424979c08ed8089a909ca23b0b007887d9b5
J Rose <josh@upros3.com>
2018-03-12 15:50:29 +0000

    added spotify ID datahelper

--- server/data-helpers.js
+++ server/data-helpers.js
@@ -21,12 +21,25 @@ module.exports = function makeDataHelpers(knex) {
           console.log(err);
         });
     },
+
+    //This needs to be edited to bring down the object being sent to only send the required information
+    //rather than the whole two tables. Same for getFriendsForums
     getSongComments: function() {
       return knex("comments")
         .leftJoin("users", "users.id", "comments.user_id")
         .where({ song_id: 1 })
         .then(results => {
           console.log("LOOK ---> " + results);
+          return results;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    getSongSpotifyId: function() {
+      return knex("songs")
+        .where({ song_id: 1 })
+        .then(results => {
           return results;
         })
         .catch(err => {
