861e8effdf652a1ea7ff3ae14437739a48a8ce5e
upRos3 <josh@upros3.com>
2018-03-13 16:31:04 -0400

    Dynamic search up and running. Commencing link to db and redux

--- src/js/components/main/Search.jsx
+++ src/js/components/main/Search.jsx
@@ -111,75 +111,37 @@ const fruit = [
   };
 })
 export default class Search extends Component {
-  //Spotify.searchTracks()...
-  componentDidMount() {
+  render() {
     const spotifyApi = new SpotifyWebApi();
 
     spotifyApi.setAccessToken(
-      "BQAbhzG8gSJ7l2psgM6aPiVOgDHhEIUCzRuIgyFDdhVbEyfLabsDBV-sraEoCfu1-R35p47BnPmVb7uQVXc"
+      "BQCqGaIpBLN3xRd9KfEzd5A4hjIA_RxlIyu8ckoz46YbcI6mROpMTb3Op-gHbTU-RgkQuOFCjt982Xb8ujk"
     );
-
-    let test = "boards canada";
-
-    spotifyApi.searchTracks(test).then(
-      data => {
-        // console.log("Searching...");
-        // console.log("----------------------------------");
-        // console.log("----------------------------------");
-        // if (data.tracks.total > 1) {
-        //   data.tracks.items.map(track => {
-        //     console.log("Artist: " + track.album.artists[0].name);
-        //     console.log("Song Name: " + track.name);
-        //     console.log("Album Name: " + track.album.name);
-        //     console.log("Album Image: " + track.album.images[2].url);
-        //     console.log("Track ID: " + track.album.artists[0].id);
-        //     console.log("Preview URL: " + track.preview_url);
-        //     console.log("----------------------------------");
-        //   });
-        // } else {
-        //   console.log("nothing found :(");
-        // }
-        // console.log("----------------------------------");
-        // console.log("Finished Search!");
-        console.log(data.tracks.items);
-        this.props.dispatch(spotifyTrackData(data.tracks.items));
-      },
-      err => {
-        console.log("Something went wrong!", err);
-      }
-    );
-  }
-  render() {
     const tracks = this.props.searchedTracks;
     let testArr = [];
     let test = tracks.map(track => {
-      // return (
-      //   <tbody>
-      //     <td>{track.album.artists[0].name}</td>
-      //     <td>{track.name}</td>
-      //     <td>{track.album.name}</td>
-      //     <td>
-      //       <img src={track.album.images[2].url} />
-      //     </td>
-      //   </tbody>
-      // );
-      testArr.push(`${track.album.artists[0].name} ${track.name}`);
+      testArr.push(
+        `${track.album.artists[0].name} - ${track.name} - ${track.album.name}`
+      );
     });
+
+    const onChange = evt => {
+      spotifyApi.searchTracks(evt).then(data => {
+        console.log(data.tracks.items);
+        this.props.dispatch(spotifyTrackData(data.tracks.items));
+      });
+    };
+
     return (
       <div>
         <AutoComplete
-          floatingLabelText="Type 'r', case insensitive"
-          filter={AutoComplete.caseInsensitiveFilter}
-          dataSource={testArr}
-        />
-        <br />
-        <AutoComplete
-          floatingLabelText="Type 'peah', fuzzy search"
+          floatingLabelText="Search for your favourite tunes:"
           filter={AutoComplete.fuzzyFilter}
           dataSource={testArr}
-          maxSearchResults={5}
+          maxSearchResults={10}
+          onUpdateInput={onChange}
+          fullWidth={true}
         />
-        <div>{test}</div>
       </div>
     );
   }
