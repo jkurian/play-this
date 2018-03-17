2491412aadac510b58fcad6a5f545fec0210b379
jkurian <jkurian770@gmail.com>
2018-03-17 02:13:34 -0400

    Refactored navbar and landingpage so buttons on navbar are separate components

---
+++ src/js/components/LogoutButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { logout } from "../actions/logout";
+import RaisedButton from 'material-ui/RaisedButton'
+
+@connect(store => {
+  return {
+  };
+})
+export default class LogoutButton extends React.Component {
+  render() {
+    const onLogoutClick = evt => {
+      this.props.dispatch(logout());
+      this.props.history.push("/login");
+    };
+    return (
+      <div style={{marginTop: 5, marginRight: 55 }}>
+        <RaisedButton label="Logout" primary={true} onClick={onLogoutClick} />
+      </div>
+    )
+  }
+}
--- src/js/components/NavBar.jsx
+++ src/js/components/NavBar.jsx
@@ -5,57 +5,22 @@ import { withRouter } from "react-router-dom";
 import AppBar from "material-ui/AppBar";
 import RaisedButton from "material-ui/RaisedButton";
 import SignUpForm from "./SignUp/SignUpForm.jsx";
-
-import { displayLoginForm, displaySignupForm } from "../actions/navbar";
-import { logout } from "../actions/logout";
+import LogoutButton from "./LogoutButton.jsx";
+import LoggedInButtons from "./LoggedInButtons.jsx";
 import { sidebarToggle } from "../actions/sidebar";
 
 @connect(store => {
   return {
-    showLoginForm: store.navbar.showLoginForm,
-    showSignupForm: store.navbar.showSignupForm,
     sessionCookie: store.login.sessionCookie,
     open: store.sidebar.open
   };
 })
 class NavBar extends React.Component {
   render() {
-    const onLoginClick = evt => {
-      console.log('login clicked');
-      
-      this.props.dispatch(displayLoginForm(!this.props.showLoginForm));
-      //prevents pushing to history if the user is already on /signup
-      if (this.props.location.pathname !== "/login") this.props.history.push("/login");
-    };
-
-    const onSignupClick = evt => {
-      this.props.dispatch(displaySignupForm(!this.props.showSignupForm));
-      //prevents pushing to history if the user is already on /signup
-      if (this.props.location.pathname !== "/signup") this.props.history.push("/signup");
-    };
-
-    const onLogoutClick = evt => {
-      this.props.dispatch(logout());
-      this.props.history.push("/login");
-    };
-
     let buttons = null;
-
-    const logoutButton = (
-      <div style={{marginTop: 5, marginRight: 55 }}>
-        <RaisedButton label="Logout" primary={true} onClick={onLogoutClick} />
-      </div>
-    );
-    const rightButtons = (
-      <div>
-        <RaisedButton label="Login" primary={true} onClick={onLoginClick} />
-        <RaisedButton label="Sign up" primary={true} onClick={onSignupClick} />
-      </div>
-    );
-
     this.props.sessionCookie
-      ? (buttons = logoutButton)
-      : (buttons = rightButtons);
+      ? (buttons = <LogoutButton />)
+      : (buttons = <LoggedInButtons />);
 
     const toggleSideBar = () => {
       this.props.dispatch(sidebarToggle(!this.props.open));
