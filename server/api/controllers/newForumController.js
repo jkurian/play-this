65153a342e97c379ec27d4994c86cbfe2122e9a3
upRos3 <josh@upros3.com>
2018-03-17 00:22:05 -0400

    Able to redirect to new forum on forum creation. Bugs still lie with the rendering.

--- server/api/controllers/newForumController.js
+++ server/api/controllers/newForumController.js
@@ -1,10 +1,9 @@
 exports.addNewForum = function(req, res, dataHelpers) {
-    let err = false;
-    dataHelpers.insertNewForum(req.body)
-    .then(result => {
-        console.log('RESULT IN NEWFORUM CONTROLLER ---->', result)
-        res.json(result);
-    })
+  let err = false;
+  dataHelpers.insertNewForum(req.body).then(result => {
+    console.log("RESULT IN NEWFORUM CONTROLLER ---->", result);
+    res.json(result);
+  });
 
-    if (err) res.status(501).send('failed');
-}
+  if (err) res.status(501).send("failed");
+};
--- src/js/components/main/Forum.jsx
+++ src/js/components/main/Forum.jsx
@@ -1,56 +1,63 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { connect } from "react-redux";
-import { withRouter } from 'react-router-dom'
+import { withRouter, Redirect } from "react-router-dom";
 
-import SideBar from './SideBar.jsx'
-import Search from './Search.jsx'
-import Post from './Post.jsx'
+import SideBar from "./SideBar.jsx";
+import Search from "./Search.jsx";
+import Post from "./Post.jsx";
 
 import { fetchSongInfo } from "../../actions/post";
-import {getRequest} from '../../actions/forum'
-import { sidebarToggleClose } from '../../actions/sidebar'
+import { getRequest } from "../../actions/forum";
+import { sidebarToggleClose } from "../../actions/sidebar";
 
 @connect(store => {
   return {
     viewingRequest: store.forum.viewingRequest,
-    sessionCookie: store.login.sessionCookie
+    sessionCookie: store.login.sessionCookie,
+    newForumLink: store.sidebar.userForums[0].id
   };
 })
 class Forum extends Component {
+  componentWillMount() {
+    this.setState({ redirectToNewPage: false });
+  }
+
   componentWillUpdate() {
     this.props.dispatch(sidebarToggleClose());
-    console.log('FORUM VIEW IS UPDATING', this.props.viewingRequest);
+    console.log("FORUM VIEW IS UPDATING", this.props.viewingRequest);
   }
   componentDidUpdate() {
     if (!this.props.sessionCookie) {
-      this.props.history.push('/login')
+      this.props.history.push("/login");
     }
   }
-    render() {
-    const titleStyle={
-      fontFamily: 'Raleway, sans-serif',
+  render() {
+    const titleStyle = {
+      fontFamily: "Raleway, sans-serif",
       marginTop: 100,
       fontSize: 50,
       fontWeight: 900,
-      textAlign: 'center'
-    }
-    const paragraphStyle={
-      fontFamily: 'Raleway, sans-serif',
+      textAlign: "center"
+    };
+    const paragraphStyle = {
+      fontFamily: "Raleway, sans-serif",
       fontSize: 30,
       fontWeight: 200,
-      textAlign: 'center'
-    }
+      textAlign: "center"
+    };
+    let newForumLink = `/forum/${this.props.newForumLink}`;
     return (
       <div>
         <SideBar />
         <h1 style={titleStyle}>{this.props.viewingRequest.title}</h1>
-        <br/>
+        <br />
         <p style={paragraphStyle}>{this.props.viewingRequest.explanation}</p>
         <Search />
         <Post />
+        <Redirect to={newForumLink} />;
       </div>
     );
   }
 }
-export default withRouter(Forum)
+export default withRouter(Forum);
